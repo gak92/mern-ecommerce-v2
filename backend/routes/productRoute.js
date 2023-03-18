0ea69248728d51c159d24bae3f97467111222e09
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-18 15:09:14 +0500

    Create ErrorHanlder class

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -11,6 +11,10 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetail);
+router
+  .route("/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
+  .get(getProductDetail);
 
 module.exports = router;
