3d8b36c5b42928b2a49e27d793bab5ca83e2150f
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-20 13:20:15 +0500

    create user login functionality and successfully login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,3 +23,30 @@ exports.registerUser = catchAsyncErrors( async(req, res, next) => {
     token,
   });
 });
+
+// Login User
+exports.loginUser = catchAsyncErrors( async(req,res,next) => {
+  const { email, password } = req.body;
+
+  // If email or password does not entered
+  if(!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+
+  const user = await User.findOne({email}).select("+password");
+  if(!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = user.comparePassword(password);
+  if(!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJWTToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
