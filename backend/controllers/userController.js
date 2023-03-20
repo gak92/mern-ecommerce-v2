e84b59a28b42c27dfda422aa06b70761c702dc89
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-20 12:19:44 +0500

    Create controller and route for the user and successfully register a user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/userModel');
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+// Register a user
+exports.registerUser = catchAsyncErrors( async(req, res, next) => {
+  const {name, email, password} = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is user public id",
+      url: "avatar profile url"
+    }
+  });
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
