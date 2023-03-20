50ba6b99e4bb3c6447ba2198aa1346a2b26a2216
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-20 12:45:44 +0500

    used jsonwebtoken to generate token when user register successfully

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,51 +1,59 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please Enter Your Name"],
     minLength: [4, "Name should have atleast 4 character"],
-    maxLength: [30, "Name cannot exceed 30 character"]
+    maxLength: [30, "Name cannot exceed 30 character"],
   },
   email: {
     type: String,
     required: [true, "Please Enter Your Email"],
     unique: true,
-    validate: [validator.isEmail, "Please Enter a valid email"]
+    validate: [validator.isEmail, "Please Enter a valid email"],
   },
   password: {
     type: String,
     required: [true, "Please Enter Your Password"],
     minLength: [8, "Password should have atleast 8 character"],
-    select: false
+    select: false,
   },
   avatar: {
     public_id: {
       type: String,
-      required: true
+      required: true,
     },
     url: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   role: {
     type: String,
-    default: "user"
+    default: "user",
   },
   resetPasswordToken: String,
-  resetPasswordExpire: Date
+  resetPasswordExpire: Date,
 });
 
 // Hashing the password before saving into the database
-userSchema.pre('save', async function(next) {
-  if(!this.isModified("password")) {
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
   }
 
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+// JWT Token
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
