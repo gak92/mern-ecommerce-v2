c5b9805dcc3d147800f554f7d2f7ffe0dc3f1892
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-22 18:48:49 +0500

    Handle mongoose duplicate key error (code=11000)

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,12 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Mongoose Duplicate Key Error
+  if(err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
