0b41717d6c03e63ec3cae3e500676ea3f8a9fb11
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-26 18:36:29 +0500

    Add functionality to add review to the product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,20 +5,25 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetail,
+  createProductReview,
 } = require("../controllers/productController");
 const { isUserAuthenticated, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+
 router
   .route("/admin/product/new")
   .post(isUserAuthenticated, authorizeRoles("admin"), createProduct);
-router
+
+  router
   .route("/admin/product/:id")
   .put(isUserAuthenticated, authorizeRoles("admin"), updateProduct)
   .delete(isUserAuthenticated, authorizeRoles("admin"), deleteProduct);
 
 router.route("/product/:id").get(getProductDetail);
+
+router.route("/review").put(isUserAuthenticated, createProductReview);
 
 module.exports = router;
