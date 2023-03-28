095c75609633ab87f185449db1fc2e9ac6e40d85
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-28 14:53:14 +0500

    Backend complete - fix order not found error

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -78,6 +78,10 @@ exports.getAllOrders = catchAsyncErrors( async(req, res, next) => {
 exports.updateOrder = catchAsyncErrors( async(req, res, next) => {
   const order = await Order.findById(req.params.id);
 
+  if(!order) {
+    return next(new ErrorHandler(`Order not found with this id: ${req.params.id}`, 404));
+  }
+
   if(order.orderStatus === "Delivered") {
     return next(new ErrorHandler("You have already delivered this order", 400));
   }
