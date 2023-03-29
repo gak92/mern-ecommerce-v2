5bfafa4466b58155474a6f3ed4f09f0fb8412ca7
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-29 20:07:50 +0500

    Install and add WebFont from webfontloader package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/layout/Header';
 import {BrowserRouter as Router} from "react-router-dom";
+import WebFont from 'webfontloader';
+
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  }, []);
+
   return (
    <Router>
     <Header />
