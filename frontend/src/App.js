885fa32798fd82dfa86047072fa60c8fda309169
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-30 23:46:45 +0500

    Add Home Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import WebFont from 'webfontloader';
 import Footer from './components/layout/Footer/Footer';
-
+import Home from './components/Home/Home';
 
 function App() {
 
@@ -19,6 +19,10 @@ function App() {
   return (
    <Router>
     <Header />
+    
+    <Routes>
+      <Route exact path='/' Component={Home} />
+    </Routes>
 
     <Footer />
    </Router>
